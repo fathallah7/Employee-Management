2b6bb4528713dc8d0fa352ac5d51a449d004b7f2
Abdullah <abdullahxoff@gmail.com>
2025-05-30 11:41:58 +0300

    crud done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use Termwind\Components\Dd;
 
 class UserController extends Controller
 {
+
+
+
+
 
     public function index()
     {
@@ -17,9 +22,13 @@ class UserController extends Controller
     }
 
 
+
+
+
+
+
     public function show($userID)
     {
-
         //select * from employees where id = $userID limit 1;
 
         $singleUserFromDB = Employee::find($userID); //model object
@@ -35,17 +44,29 @@ class UserController extends Controller
         }
 
         return view('users.info', ['info' => $singleUserFromDB]);
+
+        //     public function show(ُEmployee $user) //type hinting
+        // {
+        //     return view('users.info', ['info' => $user]);
+        // }
     }
-    //     public function show(ُEmployee $user) //type hinting
-    // {
-    //     return view('users.info', ['info' => $user]);
-    // }
+
+
+
+
+
 
 
     public function create()
     {
         return view('users.create');
     }
+
+
+
+
+
+
 
 
     public function store()
@@ -62,36 +83,79 @@ class UserController extends Controller
 
         // 2- Store Data in Database
 
+        Employee::create([
+            'first_name' => $name,
+            'last_name' => $name2,
+            'email' => $email,
+            'xyz' => 'ssss'   // will ignore it - bec. not in the fillable 
+        ]);
+
+        // $employee = new Employee;
+        // $employee->first_name = $name;
+        // $employee->last_name = $name2;
+        // $employee->email = $email;
+        // $employee->save();
 
         // 3- redirection to index
         return to_route('index');
     }
 
 
-    public function edit()
+
+
+
+
+
+
+    public function edit($userID)
     {
-        return view('users.edit');
+        $user = Employee::find($userID);
+        return view('users.edit', ['info' => $user]);
     }
 
 
-    public function update()
+
+
+
+    public function update($userID)
     {
+        // dd($userID);
+
         // 1- Get User Data
         $name = request()->firstName;
         $name2 = request()->lastName;
         $email = request()->email;
 
-        // 2- Store Data in Database
+        // 2- Update Data in Database
+        $singleUserFromDB = Employee::find($userID);
 
+        $singleUserFromDB->update([
+            'first_name' => $name,
+            'last_name' => $name2,
+            'email' => $email
+        ]);
+
+        // $Employee = Employee::updateOrCreate(
+        //     ['id' => $userID] ,
+        //     ['first_name' => $name, 'last_name' => $name2 , 'email' => $email]
+        // );
 
         // 3- redirection to index
         return to_route('index');
     }
 
 
-    public function destroy()
+
+
+
+
+    public function destroy($userID)
     {
         // 1- Delete User From Database
+
+        $user = Employee::find($userID);
+
+        $user->delete();
 
         // 2- redirection to index
         return to_route('index');
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -13,20 +13,20 @@
           <div class="card shadow-sm border-0">
             <div class="card-body p-4">
               <h2 class="text-center mb-4 fw-bold text-primary">Update Info</h2>
-              <form method="POST" action="{{ route('users.update', 1) }}">
+              <form method="POST" action="{{ route('users.update', $info->id) }}">
                 @csrf
                 @method('PUT')
                 <div class="mb-3">
                   <label for="firstName" class="form-label fw-semibold">First Name</label>
-                  <input type="text" name="firstName" class="form-control" id="firstName" placeholder="Enter first name">
+                  <input type="text" name="firstName" class="form-control" id="firstName" value="{{ $info->first_name }}">
                 </div>
                 <div class="mb-3">
                   <label for="lastName" class="form-label fw-semibold">Last Name</label>
-                  <input type="text" name="lastName" class="form-control" id="lastName" placeholder="Enter last name">
+                  <input type="text" name="lastName" class="form-control" id="lastName" value="{{ $info->last_name }}">
                 </div>
                 <div class="mb-3">
                   <label for="email" class="form-label fw-semibold">Email</label>
-                  <input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
+                  <input type="email" name="email" class="form-control" id="email" value="{{ $info->email }}">
                 </div>
                 <div class="d-grid">
                   <button type="submit" name="submit" class="btn btn-primary btn-lg rounded-pill shadow-sm">
